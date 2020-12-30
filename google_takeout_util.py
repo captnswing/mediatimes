e6629cfe1c1ea8776a502abe71d62bc324c0d26d
Frank Hoffsümmer <frank.hoffsummer@easypark.net>
2020-12-30 13:08:42 +0100

    cleanup
    Change-Id: I616d4eb8924fee66becc1a6926d40dab472f3851

--- google_takeout_util.py
+++ google_takeout_util.py
@@ -75,68 +75,11 @@ def find_extensions():
     print(sorted(extensions))
 
 
-def _run_exiftool_videos(fn, d=None):
-    if not d:
-        data = json.loads(open(f"{fn}.json").read())
-        d = arrow.get(data["photoTakenTime"]["timestamp"], "X")
-    s = subprocess.check_output(
-        [
-            "exiftool",
-            "-m",
-            "-tagsFromFile",
-            "%d/%f.%e.json",
-            "-overwrite_original",
-            "-progress",
-            "-GPSAltitude<GeoDataAltitude",
-            "-GPSLatitude<GeoDataLatitude",
-            "-GPSLatitudeRef<GeoDataLatitude",
-            "-GPSLongitude<GeoDataLongitude",
-            "-GPSLongitudeRef<GeoDataLongitude",
-            "-Keywords<Tags",
-            "-Subject<Tags",
-            "-Caption-Abstract<Description",
-            "-ImageDescription<Description",
-            f"-CreateDate<{d}",
-            fn,
-        ]
-    )
-    print(s)
-
-
-def _run_exiftool_images(fn):
-    command = [
-        "exiftool",
-        "-d",
-        "%s",
-        "-tagsFromFile",
-        "%d/%f.%e.json",
-        "-overwrite_original",
-        "-progress",
-        "-GPSAltitude<GeoDataAltitude",
-        "-GPSLatitude<GeoDataLatitude",
-        "-GPSLatitudeRef<GeoDataLatitude",
-        "-GPSLongitude<GeoDataLongitude",
-        "-GPSLongitudeRef<GeoDataLongitude",
-        "-Keywords<Tags",
-        "-Subject<Tags",
-        "-Caption-Abstract<Description",
-        "-ImageDescription<Description",
-        "-DateTimeOriginal<PhotoTakenTimeTimestamp",
-        fn,
-    ]
-    # print(" ".join(command))
-    try:
-        s = subprocess.check_output(command, encoding="utf-8")
-    except subprocess.CalledProcessError:
-        pass
-    print(s)
-
-
 def move_subfolders_into_batches(dir):
     os.chdir(dir)
     dircontents = os.listdir(".")
     dircontents = [d for d in dircontents if not d.startswith("batch_")]
-    batchnumber = 10
+    batchnumber = 14
     batchsize = int(len(dircontents) / batchnumber) + 1
     for i in range(batchnumber):
         batchdir = f"batch_{i + 1:03d}"
@@ -154,6 +97,6 @@ if __name__ == "__main__":
     os.chdir(DATA_DIR)
     # download_archives()
     # verify_archives("20201123T165802Z", 32, "tgz")
-    # extract_archives("zip")
-    find_extensions()
+    extract_archives("zip")
+    # find_extensions()
     # move_subfolders_into_batches('Google Photos')
